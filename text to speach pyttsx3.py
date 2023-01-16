2aaba05504db262e48e2836ca79eaf599f546548
Cherif IGUI <77020986+3803531@users.noreply.github.com>
2023-01-16 08:11:25 +0100

    premier prototype de text to speach avec pyttsx3
    Il existe  pyttsx3, une bibliothèque qui utilise les moteurs
     de synthèse de la plateforme pour générer la synthèse vocale.

---
+++ text to speach pyttsx3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 16 02:38:58 2023
+
+@author: Cherif IGUI 
+
+Il existe  pyttsx3, une bibliothèque qui utilise les moteurs
+ de synthèse de la plateforme pour générer la synthèse vocale.
+"""
+
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("Bonjour, je m'appel cherif ?")
+engine.runAndWait()
+
+
